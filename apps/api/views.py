7b06ed749c98f6c3272684ee397c71d8f8a2362d
Ben Steel <bsteel300@gmail.com>
2020-10-27 13:00:48 -0700

    basic api functional

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import View
+from django.http import JsonResponse
 import random, json
 from datetime import datetime, timezone
 
@@ -7,14 +7,20 @@ def create_random_field():
     maximum = 100
     return random.random()*maximum
 
-class ApiView(View):
-    def get(self, request):
+
+def api(request):
+    if request.method == 'GET':
         n = datetime.now(tz=timezone.utc)
         
-        obj = {
+        data = {
             'timestamp': n.isoformat().replace('+00:00', 'Z')
         }
+        
         for i in range(random.randint(0,10)):
-            obj[str(i)] = create_random_field()
+            data[str(i)] = create_random_field()
 
-        return json.dumps(obj)
+        
+
+        return JsonResponse(data)
+    else: 
+        return render(request, 'api/post.html', {})
